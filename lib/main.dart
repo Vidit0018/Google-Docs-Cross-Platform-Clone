7160eb2349623cb52dd3d657eb664f62223910af
vidit0018 <vidit.msit@gmail.com>
2025-01-16 23:03:13 +0530

    Route Master installed successfully

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:docs_clone/models/error_model.dart';
 import 'package:docs_clone/repository/auth_repository.dart';
+import 'package:docs_clone/router.dart';
 import 'package:docs_clone/screens/home_screen.dart';
 import 'package:docs_clone/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:routemaster/routemaster.dart';
 
 void main() {
   runApp(const ProviderScope(child:  MyApp()));
@@ -36,13 +38,21 @@ class _MyAppState extends ConsumerState<MyApp> {
   @override
   Widget build(BuildContext context) {
     final user =ref.watch(userProvider);
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: user ==null ? const LoginScreen() :  HomeScreen(),
+      routerDelegate: RoutemasterDelegate(routesBuilder: (context){
+        final user= ref.watch(userProvider);
+        if(user!=null && user.token.isNotEmpty){
+          return loggedInRoute;
+        }
+        return loggedOutRoute;
+      }),
+      routeInformationParser:  const RoutemasterParser(),
+      // home: user ==null ? const LoginScreen() :  HomeScreen(),
     );
   }
 }
---
+++ lib/router.dart
@@ -0,0 +1,17 @@
+import 'package:docs_clone/screens/home_screen.dart';
+import 'package:docs_clone/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:routemaster/routemaster.dart';
+
+final loggedOutRoute = RouteMap(routes: {
+  '/' :( (route) => MaterialPage(child: LoginScreen())) ,
+  
+
+}
+);
+final loggedInRoute = RouteMap(routes: {
+  '/' :( (route) => MaterialPage(child: HomeScreen())) ,
+
+
+}
+);
